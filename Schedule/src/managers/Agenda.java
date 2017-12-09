e51a7e4014eb19e57893f5f09aab318cc111a3b9
tvarano54 <tvarano54@gmail.com>
2017-12-09 15:36:15 -0500

    update build

--- Schedule/src/managers/Agenda.java
+++ Schedule/src/managers/Agenda.java
@@ -37,7 +37,7 @@ public class Agenda extends JPanel
 {
    private static final long serialVersionUID = 1L;
    public static final String APP_NAME = "Agenda";
-   public static final String BUILD = "v1.5.7 ß";
+   public static final String BUILD = "v1.6.1 ß";
    public static final int MIN_W = 733, MIN_H = 360;
    public static final int PREF_W = MIN_W, PREF_H = 460;
    private PanelManager manager;
