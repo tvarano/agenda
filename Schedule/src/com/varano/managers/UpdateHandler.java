2ec873de7d0afaa78ef524c1942479d364de9e5c
tvarano54 <tvarano54@gmail.com>
2018-10-22 20:11:39 -0400

    small changes

--- Schedule/src/com/varano/managers/UpdateHandler.java
+++ Schedule/src/com/varano/managers/UpdateHandler.java
@@ -90,7 +90,7 @@ public class UpdateHandler {
       Agenda.log("asking for update permission");
       return JOptionPane.showConfirmDialog(null,
             "An update is available.\nWould you like to install?"
-                  + "\nAgenda will need to restart.",
+                  + "\n" + Agenda.APP_NAME + " will need to restart.",
             Agenda.APP_NAME, JOptionPane.YES_NO_OPTION,
             JOptionPane.QUESTION_MESSAGE, null) == 0;
    }
